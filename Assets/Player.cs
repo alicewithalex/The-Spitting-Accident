8435cb38a358184285ac49fd34748a01dd736a87
alicewithalex <alicewithalex25@gmail.com>
2020-08-07 12:21:14 +0300

    Player Movement and Rotation
    Added Player Possibility To move and rotate

---
+++ Assets/Player.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody),typeof(Animator))]
+public class Player : MonoBehaviour
+{
+
+    #region Public Variables
+
+    public float movementSpeed = 10f;
+    public float rotationSpeed = 10f;
+
+    #endregion
+    
+    #region Serialized Variables
+
+    
+
+    #endregion
+    
+    #region Private Variables
+
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+    private Transform _camera;
+
+    private Vector2 input;
+    private float smoothMagnitude = 0f;
+    private bool weaponEnabled = false;
+
+    #endregion
+
+
+    #region Unity's Methods
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        
+        if (Camera.main == null)
+        {
+            _camera = FindObjectOfType<Camera>().transform;
+        }
+        else _camera = Camera.main.transform;
+        
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleAnimation();
+    }
+
+    private void FixedUpdate()
+    {
+        PerformRotation();
+        PerformMovement();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void HandleInput()
+    {
+        input = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+    }
+
+    private void HandleAnimation()
+    {
+        smoothMagnitude = Mathf.Lerp(smoothMagnitude, input.normalized.magnitude, Time.deltaTime * 5f);
+        _animator.SetFloat("speedPercentage",smoothMagnitude);
+    }
+    
+    private void PerformRotation()
+    {
+        Vector2 inputDirection = input.normalized;
+
+        if (inputDirection != Vector2.zero)
+        {
+            _rigidbody.rotation = Quaternion.Slerp(_rigidbody.rotation, Quaternion.AngleAxis(
+                                                                            (Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg +
+                                                                             _camera.eulerAngles.y), Vector3.up)*Quaternion.Euler(_rigidbody.transform.eulerAngles.x,0f,_rigidbody.transform.eulerAngles.z), Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    private void PerformMovement()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _rigidbody.transform.forward*movementSpeed*Time.fixedDeltaTime*input.magnitude);
+    }
+
+    #endregion
+    
+    #region Public Methods
+
+    
+
+    #endregion
+}
---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+
+{
+    [SerializeField] private Transform target;
+
+    public Vector3 offset;
+    public float smoothTime = 5f;
+    
+    public float mouseSensivity;
+    public Vector2 angleRange;
+
+    private float xRot, yRot;
+    private Vector3 currentVelocity,currentRotation;
+    
+    private void Start()
+    {
+        PerformOffset();
+    }
+
+    private void Update()
+    {
+        // HandleInput();
+    }
+
+    private void LateUpdate()
+    {
+        // PerformRotation();
+        PerformOffset();
+    }
+
+    private void PerformOffset()
+    {
+        transform.position = target.position - transform.forward * offset.z - transform.right * offset.x -
+                             transform.up * offset.y;
+    }
+
+    private void PerformRotation()
+    {
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(xRot, yRot, 0f), ref currentVelocity,
+            Time.deltaTime * smoothTime);
+
+        transform.eulerAngles = currentRotation;
+    }
+
+    private void HandleInput()
+    {
+        xRot -= Input.GetAxisRaw("Mouse Y")*mouseSensivity;
+        yRot += Input.GetAxisRaw("Mouse X")*mouseSensivity;
+
+        xRot = Mathf.Clamp(xRot, angleRange.x, angleRange.y);
+    }
+}
